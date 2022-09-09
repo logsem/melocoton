dc2d4afd8878079292f41f1919ac2345de010158
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2022-09-09 18:36:15 +0200

    fix/tweak

--- theories/c_toy_mlang/class_instances.v
+++ theories/c_toy_mlang/class_instances.v
@@ -18,14 +18,11 @@ Proof.
   eapply mlanguage.fill_val, Hsub. by rewrite /= fill_app.
 Qed.
 
+Definition only {A} (x : A) : A → Prop :=
+  λ y, y = x.
+
 Section pure_exec.
   Context {p:program}.
-  (* Local Ltac solve_exec_safe := intros; subst; do 4 eexists; try (econstructor; eauto; done). *)
-  (* Local Ltac solve_exec_puredet := simpl; intros; inv_head_step; try done. *)
-  (* Local Ltac solve_pure_exec := *)
-  (*   subst; intros ?; apply nsteps_once, pure_head_step_pure_step; *)
-  (*     constructor; [solve_exec_safe | solve_exec_puredet]. *)
-
   Local Ltac solve_exec_safe :=
     intros; subst; try (exists (λ _, True); econstructor; eauto; done).
   Local Ltac solve_exec_puredet :=
@@ -39,22 +36,22 @@ Section pure_exec.
     [apply bool_decide_eq_true_1 in H| apply bool_decide_eq_false_1 in H]).
 
   Global Instance pure_let x (v1 v2 : val) :
-    PureExec True 1 p (Let x (Val v1) (Val v2)) (singl (subst' x v1 v2)).
+    PureExec True 1 p (Let x (Val v1) (Val v2)) (only (subst' x v1 v2)).
   Proof. solve_pure_exec. Qed.
 
   Global Instance pure_unop op v v' :
-    PureExec (un_op_eval op v = Some v') 1 p (UnOp op (Val v)) (singl (Val v')).
+    PureExec (un_op_eval op v = Some v') 1 p (UnOp op (Val v)) (only (Val v')).
   Proof. solve_pure_exec. Qed.
 
   Global Instance pure_binop op v1 v2 v' :
-    PureExec (bin_op_eval op v1 v2 = Some v') 1 p (BinOp op (Val v1) (Val v2)) (singl (Val (LitV v'))) | 10.
+    PureExec (bin_op_eval op v1 v2 = Some v') 1 p (BinOp op (Val v1) (Val v2)) (only (Val (LitV v'))) | 10.
   Proof. solve_pure_exec. Qed.
 
   (* Higher-priority instance for [EqOp]. *)
   Global Instance pure_eqop v1 v2 :
     PureExec (vals_compare_safe v1 v2) 1 p
       (BinOp EqOp (Val (LitV v1)) (Val (LitV v2)))
-      (singl (Val $ LitV $ LitBool $ bool_decide (v1 = v2))) | 1.
+      (only (Val $ LitV $ LitBool $ bool_decide (v1 = v2))) | 1.
   Proof.
     intros Hcompare.
     cut (bin_op_eval EqOp (LitV v1) (LitV v2) = Some $ LitBool $ bool_decide (v1 = v2)).
@@ -66,39 +63,39 @@ Section pure_exec.
   Qed.
 
   Global Instance pure_if_true v e1 e2 : asTruth v = true ->
-    PureExec True 1 p (If (Val $ v) e1 e2) (singl e1).
+    PureExec True 1 p (If (Val $ v) e1 e2) (only e1).
   Proof. intros H. solve_pure_exec. Qed.
   Global Instance pure_if_non_zero (v0:Z) e1 e2 : v0 ≠ 0%Z ->
-    PureExec True 1 p (If (Val $ LitV $ LitInt v0) e1 e2) (singl e1).
+    PureExec True 1 p (If (Val $ LitV $ LitInt v0) e1 e2) (only e1).
   Proof. intros H. apply pure_if_true. destruct v0; cbn; congruence. Qed.
   Global Instance pure_if_true_bool e1 e2 :
-    PureExec True 1 p (If (Val $ LitV $ LitBool true) e1 e2) (singl e1).
+    PureExec True 1 p (If (Val $ LitV $ LitBool true) e1 e2) (only e1).
   Proof. apply pure_if_true. easy. Qed.
   Global Instance pure_if_false v e1 e2 : asTruth v = false ->
-    PureExec True 1 p (If (Val $ v) e1 e2) (singl e2).
+    PureExec True 1 p (If (Val $ v) e1 e2) (only e2).
   Proof. intros H. solve_pure_exec. Qed.
   Global Instance pure_if_zero e1 e2 :
-    PureExec True 1 p (If (Val $ LitV $ LitInt (0%Z)) e1 e2) (singl e2).
+    PureExec True 1 p (If (Val $ LitV $ LitInt (0%Z)) e1 e2) (only e2).
   Proof. apply pure_if_false. easy. Qed.
   Global Instance pure_if_false_bool e1 e2 :
-    PureExec True 1 p (If (Val $ LitV $ LitBool false) e1 e2) (singl e2).
+    PureExec True 1 p (If (Val $ LitV $ LitBool false) e1 e2) (only e2).
   Proof. apply pure_if_zero. Qed.
 
   Global Instance pure_while e1 e2 :
-    PureExec True 1 p (While e1 e2) (singl (If e1 (Let BAnon e1 (While e1 e2)) (Val $ LitV $ LitInt 0))).
+    PureExec True 1 p (While e1 e2) (only (If e1 (Let BAnon e1 (While e1 e2)) (Val $ LitV $ LitInt 0))).
   Proof. solve_pure_exec. Qed.
 
   Global Instance pure_funcall s va args res e :
     PureExec ((p : gmap.gmap string function) !! s = Some (Fun args e) ∧ zip_args args va = Some res) 1 p
              (FunCall (Val $ LitV $ LitFunPtr s) (map Val va))
-             (singl (subst_all res e)).
+             (only (subst_all res e)).
   Proof.
     solve_pure_exec; destruct H as [H1 H2].
     { exists (λ _, True). econstructor; eauto.
       rewrite /apply_function H2//. }
     { econstructor; eauto.
       - rewrite /apply_function H2//.
-      - rewrite /singl //. }
+      - rewrite /only //. }
   Qed.
 
 End pure_exec.
