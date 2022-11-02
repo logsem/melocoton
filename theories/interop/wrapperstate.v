6433e607f16f479b76d6bb701d34fb24547d7b8a
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2022-11-02 15:34:52 +0100

    wrapper: a few more comments

--- theories/interop/wrapperstate.v
+++ theories/interop/wrapperstate.v
@@ -4,7 +4,6 @@ From melocoton.interop Require Import basics.
 
 (* This defines the wrapper private state. Like the whole wrapper state (see
    `state` in wrappersem.v), it can be either:
-
    - a `wrapstateML` at a boundary when interacting with another ML-like
      component;
    - a `wrapstateC` when doing internal steps (ie executing wrapped C code).
@@ -13,10 +12,16 @@ From melocoton.interop Require Import basics.
 Section wrapperstate.
 
 Record wrapstateC : Type := WrapstateC {
+  (* ML location → logical location *)
   χC : lloc_map;
+  (* logical (block-level) store *)
   ζC : lstore;
+  (* logical location → C address.
+     Changes when the GC runs and moves/deallocates blocks. *)
   θC : addr_map;
+  (* addresses in C memory that are registered as roots *)
   rootsC : gset addr;
+  (* the remaining piece of ML store not accessible from C *)
   privσC : store;
 
   χC_injective : ∀ ℓ1 ℓ2 γ,
@@ -31,9 +36,14 @@ Record wrapstateC : Type := WrapstateC {
 }.
 
 Record wrapstateML : Type := WrapstateML {
+  (* ML location → logical locations *)
   χML : lloc_map;
+  (* logical (block level) store *)
   ζML : lstore;
+  (* C address → logical location tracked by the root registered at this
+     address. *)
   rootsML : roots_map;
+  (* the remaining piece of C store not accessible from ML *)
   privmemML : memory;
 
   χML_injective : ∀ ℓ1 ℓ2 γ,
