092cb21c29a0a5f9c58a4a7ab094f1bd41678a01
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2023-04-14 00:19:28 +0200

    tweak setup commands

--- tools/setup-artifact.sh
+++ tools/setup-artifact.sh
@@ -6,11 +6,7 @@ opam repo add -y iris-dev git+https://gitlab.mpi-sws.org/iris/opam.git
 opam update
 opam install -y coq.8.16.1 coq-autosubst.dev
 opam pin -y ./iris-parametric-index
-
-cd transfinite-parametric-stepindex
-make -j8
-make install
-cd ..
+opam pin -y ./transfinite-parametric-stepindex
 
 cd melocoton
 make
