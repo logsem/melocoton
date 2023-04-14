612181736ecfe8a24f420b05dfab8c7d9f960861
Johannes Hostert <s8johost@stud.uni-saarland.de>
2023-04-14 10:39:48 +0200

    adapt example to paper

--- examples/minibigarray/minibigarray.ml
+++ examples/minibigarray/minibigarray.ml
@@ -6,12 +6,12 @@ external buf_free   : buf -> unit                                 = "buf_free"
 external buf_get    : buf -> int -> char                          = "buf_get"
 external buf_upd    : int -> int -> (int -> char) -> buf -> unit  = "buf_upd"
 
-external snappy_compress               : buf -> buf -> bool       = "wrap_compress"
-external snappy_max_compressed_length  : int -> int               = "wrap_max_len"
+external wrap_compress               : buf -> buf -> bool       = "wrap_compress"
+external wrap_max_len  : int -> int               = "wrap_max_len"
 
 let is_compressible (xs: char array) =
-  let len = Array.length xs in
-  let (inp, outp) = (buf_alloc len, buf_alloc (snappy_max_compressed_length len)) in
-  buf_upd 0 (len - 1) (fun i -> Array.get xs i) inp; 
-  let _ = snappy_compress inp outp in let shrank = outp.used < inp.used in
+  let len = Array.length xs in if len = 0 then false else
+  let (inp, outp) = (buf_alloc len, buf_alloc (wrap_max_len len)) in
+  buf_upd 0 (len - 1) (fun i -> Array.get xs i) inp;
+  let _ = wrap_compress inp outp in let shrank = outp.used < inp.used in
   buf_free inp; buf_free outp; shrank
--- examples/minibigarray/minibigarray.mli
+++ examples/minibigarray/minibigarray.mli
@@ -1,12 +1,12 @@
 type buf
 
 
-val buf_alloc  : int -> buf   
-val buf_free   : buf -> unit                             
-val buf_get    : buf -> int -> char                      
-val buf_upd    : int -> int -> (int -> char) -> buf -> unit 
+val buf_alloc  : int -> buf
+val buf_free   : buf -> unit
+val buf_get    : buf -> int -> char
+val buf_upd    : int -> int -> (int -> char) -> buf -> unit
 
-val snappy_compress               : buf -> buf -> bool     
-val snappy_max_compressed_length  : int -> int            
+val wrap_compress               : buf -> buf -> bool
+val wrap_max_len  : int -> int
 
 val is_compressible : char array -> bool
