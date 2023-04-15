d8e946e772d300e080bc4f8073225ac752c7704d
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2023-04-15 09:06:59 +0200

    tweak setup-artifact.sh

--- tools/setup-artifact.sh
+++ tools/setup-artifact.sh
@@ -8,7 +8,7 @@ opam switch -y create . ocaml-base-compiler.4.14.1
 eval $(opam env)
 opam repo add -y iris-dev git+https://gitlab.mpi-sws.org/iris/opam.git
 opam update
-opam install -y coq.8.16.1 coq-autosubst.dev
+opam install -y coq.8.16.1
 opam pin -y ./iris-parametric-index
 opam pin -y ./transfinite-parametric-stepindex
 opam pin -y coq-autosubst ./autosubst
