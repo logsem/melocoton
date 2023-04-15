e303b480bf98b6cb64f5a75c1071d18ddd5648e0
Johannes Hostert <s8johost@stud.uni-saarland.de>
2023-04-15 12:46:33 +0200

    Update landin's knot in Code to use assert false

--- examples/landin/landin.ml
+++ examples/landin/landin.ml
@@ -1,6 +1,6 @@
 
 external callk : ('a -> 'b) ref -> 'a -> 'b = "callk"
 let knot (f : ('a -> 'b) -> ('a -> 'b)) =
-  let l = ref (fun x -> Obj.magic x) in (* use of Obj.magic here is to quiet the type checker -- it is never executed *)
+  let l = ref (fun _ -> assert false) in
   l := (fun x -> f (fun y -> (callk l y)) x);
   (fun x -> callk l x)
